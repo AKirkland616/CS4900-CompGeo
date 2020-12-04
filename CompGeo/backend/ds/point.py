3856e47425cf9128c56bc5d7d47c5f4e4e2cd020
LorandMezei <lorand_mezei@yahoo.com>
2020-12-03 19:31:53 -0800

    Initial point class and point tests

---
+++ CompGeo/backend/ds/point.py
@@ -0,0 +1,14 @@
+class Point:
+	x, y = 0,0 # Two-dimensional point (x,y).
+
+	def __init__(self, x1, y1): 
+		self.x = x1
+		self.y = y1
+
+	def getX(self):
+		return self.x
+
+	def getY(self):
+		return self.y
+
+
