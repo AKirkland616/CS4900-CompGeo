30b1b990ac29921bde623f63ca83f2111307b5d6
Synthabit <jeremymichevans@gmail.com>
2020-12-04 14:02:13 -0500

    Fixed unit testing issue for the Point and LineSegment classes.

--- CompGeo/backend/ds/LineSegment.py
+++ CompGeo/backend/ds/LineSegment.py
@@ -1,26 +1,32 @@
 from CompGeo.backend.ds.point import Point
 
-
 class LineSegment(object):
-
     # create line segment from two different points or return -1
     def __new__(cls, point1, point2):
-        if isinstance(point1, Point) and isinstance(point2, Point) and point1 != point2:
-            return super(LineSegment, cls).__new__(cls, point1, point2)
+        if isinstance((point1, point2), Point) and point1 != point2:
+            return super(LineSegment, cls).__new__(cls)
+
         else:
             return -1
 
-    # inits line segment from 2 points
     def __init__(self, point1, point2):
+        '''
+        Inits line segment from 2 points.
+        '''
         self.a = point1
         self.b = point2
 
-    # prints line segment in an understandable way. can be rewritten easily
     def __str__(self):
+        '''
+        Prints line segment in an understandable way. can be rewritten easily.
+        '''
         return "[("+str(self.a.x) + ", " + str(self.a.y) + ") -> ("+str(self.b.x) + ", " + str(self.b.y) + ")]"
 
-    # compares line segments. Returns true if equal, or false if not.
     def __eq__(self, other):
+        '''
+        Compares line segments. Returns true if equal, or false if not.
+        '''
         if isinstance(other, LineSegment):
             return (self.a == other.a and self.b == other.b) or (self.a == other.b and self.b == other.a)
+
         return False
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS4900-CompGeo
-Computational Geometry Practice in Python for CS 4900 at WMU
+Computational Geometry Practice in Python for CS 4900 at WMU.
+
+## Getting Started
